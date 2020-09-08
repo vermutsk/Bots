1999fa45071ab4c093b07b8fe0350063b1987a62
vermutsk <krnvitman@gmail.com>
2020-09-08 18:50:38 +1000

    Update telebot.py

--- telebot.py
+++ telebot.py
@@ -1,1 +1,33 @@
-print('no')
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+from config import TOKEN
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+Base = declarative_base()
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Привет!\nНапиши мне что-нибудь!")
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)  
+
+class MediaIds(Base):
+    __tablename__ = 'Media ids'
+    id = Column(Integer, primary_key=True)
+    file_id = Column(String(255))
+    filename = Column(String(255))  
+
+if __name__ == '__main__':
+    executor.start_polling(dp)    
