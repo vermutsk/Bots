9ba08c1538b5d2010cc3ee92e536ab2499378695
vermutsk <krnvitman@gmail.com>
2021-01-07 05:29:30 +1000

    Create weebhook.py

---
+++ weebhook.py
@@ -0,0 +1,11 @@
+  
+import requests
+from config import TOKEN
+
+
+def set_weebhook():
+    res=requests.get(
+        f'https://api.telegram.org/bot{TOKEN}/setWebhook',
+        params={
+                'url':'https://89.223.95.82:443/KB_8118_Bot'},
+        files={'certificate':open('webhook_cert.pem','rb')}).json()
