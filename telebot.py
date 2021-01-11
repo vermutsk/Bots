e261c1dc474ff4558cee4ba87dc852ba792ad40d
vermutsk <krnvitman@gmail.com>
2021-01-12 00:01:39 +1000

    Update telebot.py

--- telebot.py
+++ telebot.py
@@ -57,8 +57,8 @@ async def process_help_command(msg: types.Message, state: FSMContext):
 @dp.message_handler(commands=['edit'], state = '*')
 async def admin_command(msg: types.Message, state: FSMContext):
     user_id = msg.from_user.id
-    acsess = bot.get_chat_member(msg.chat.id, user_id).status
-    if acsess == 'administrator' or acsess == 'creator':
+    acsess = bot.get_chat_member(msg.chat.id, user_id)
+    if acsess.status == 'administrator' or acsess.status == 'creator':
         await state.set_state(States.ADMIN)
         t = Timer(600, save_adm(user_id, state))
         t.start()
