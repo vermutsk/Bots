fe76fb0746223beb93c94820534a8aae1a035fed
vermutsk <krnvitman@gmail.com>
2021-01-11 20:44:19 +1000

    Update functions.py

--- functions.py
+++ functions.py
@@ -86,6 +86,19 @@ def parser():
     adm_collection.insert_many(list_of_posts)
     list_of_posts.clear()
 
+def save_adm(user_id, state):
+    new_collection.remove({})
+    docs = adm_collection.find({},{'_id' : 0,'edited': 0})
+    full = []
+    for doc in docs:
+        if 'admin_id' in doc:
+            if doc['admin_id'] == str(user_id):
+                adm_collection.update_one({'doljname' : doc['doljname']}, {"$unset": {'admin_id' : 1}})
+            doc.pop('admin_id')
+        full.append(doc)
+    new_collection.insert_many(full)
+    await state.finish()
+
 def db_list(js):
     many_doc = []
     one_doc = []
